1ed99b8f0c5f529057ff65c632a7d67e0cc703cb
Albat93 <9929@holbertonstudents.com>
2024-10-21 09:34:31 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,7 @@
 
 int main()
 {
-    char str1[] = "Programming is like building a multilingual puzzle"
-
-    puts(str1);
-
-    return (0);
+    puts("Programming is like building a multilingual puzzle");
+    getchar();
+    return 0;
 }
