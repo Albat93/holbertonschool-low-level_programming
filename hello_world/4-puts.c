ef6bd3fecfa794eea1b190b23869cc6b7f5dd5bd
Albat93 <9929@holbertonstudents.com>
2024-10-21 09:14:19 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-int puts(char "Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main(void)
+{
+    char* str1 = "Programming is like building a multilingual puzzle";
+    puts(srt1);
+    return(0);
+}
