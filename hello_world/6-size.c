5a7a3c51257c7ab191934f9645d5af15e2b40035
Albat93 <9929@holbertonstudents.com>
2024-10-21 11:35:37 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <bits/libc-header-start.h>
 /**
  * main - Entry point
  *
@@ -10,10 +9,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char = 1));
-printf("Size of an int: %lu byte(s)\n", sizeof(int = 4));
-printf("Size of a long unt: %lu byte(s)\n", sizeof(long int = 4));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int = 8));
-printf("Size of a float: %lu byte(s)", sizeof(float = 4);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long unt: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+ printf("Size of a float: %lu byte(s)", sizeof(float));
 return (0);
 }
