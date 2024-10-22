9d2ebcdb46061a13ad6f71c5aa7c0c5e1544d47c
Albat93 <9929@holbertonstudents.com>
2024-10-22 10:53:45 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void);
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+{
+
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
