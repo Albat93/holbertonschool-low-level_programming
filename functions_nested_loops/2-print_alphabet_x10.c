3246d1e3048420a392d43ca35c475c7b0d67aa25
Albat93 <9929@holbertonstudents.com>
2024-10-23 14:17:51 +0000

    alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print alphabet x10
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
