a39e386f0923c4402728e1e36cd390755e08e452
Albat93 <9929@holbertonstudents.com>
2024-10-23 15:11:53 +0000

    is alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * int _isalpha(int c) - check if c is lowercase or uppercase.
+ * function that check for lowercase or uppercase character.
+ * return - always 0
+ */
+
+int _isalpha(int c)
+
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ functions_nested_loops/4-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * int r - return result of function
+ * int _isalpha(int c); - function to check for alphabetic character are lower or uppercase. 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
