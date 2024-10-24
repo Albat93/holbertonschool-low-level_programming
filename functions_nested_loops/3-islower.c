0753ab7debc85976e43c9a9d964be76095be84c6
Albat93 <9929@holbertonstudents.com>
2024-10-24 11:21:12 +0000

    is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * int _islower(int c) - check if c is lowercase.
- * function that check for lowercase character.
- * return - always 0
+ * int _islower - check if c is lowercase.
  */
 
 int _islower(int c)
