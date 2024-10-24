c1174260f6a282c0df6d1a08437a472f8f2c0388
Albat93 <9929@holbertonstudents.com>
2024-10-24 12:06:52 +0000

    Test

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * int _isalpha - check if c is lowercase or uppercase.
+ * _isalpha - check if c is lowercase or uppercase.
  */
 
 int _isalpha(int c)
 {
-	if (test_isalpha(c))
+	if (isalpha(c))
 	{
 		return (1);
 	}
