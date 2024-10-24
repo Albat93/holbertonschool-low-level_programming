d6a55c5ea1a7bf9414bc8ac1a8f51d1fbc3e16c6
Albat93 <9929@holbertonstudents.com>
2024-10-24 12:34:34 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,20 +6,19 @@
  * return:  - print + if greater, - if less, 0 if 0.
  */
 
- int print_sign(int n)
- {
-	 if (n > 0)
-	 {
-		 putchar(43);
-	 }
-	 else if (n < 0)
-	 {
-		 putchar(45);
-	 }
-	 else if (n == 0)
-	 {
-		 putchar(48);
-	 }
-	 return (0);
-
- }
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+}
+else if (n < 0)
+{
+_putchar(45);
+}
+else if (n == 0)
+{
+_putchar(48);
+}
+return (0);
+}
