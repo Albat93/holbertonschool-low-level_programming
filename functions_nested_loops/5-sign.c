155e8c2ea7e1944f275d2081ff4dbce4e8ccec02
Albat93 <9929@holbertonstudents.com>
2024-10-24 12:37:17 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+_putchar(+);
 }
 else if (n < 0)
 {
-_putchar('-');
+_putchar(-);
 }
 else if (n == 0)
 {
-_putchar('0');
+_putchar(0);
 }
 return (0);
 }
