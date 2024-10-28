875261dd09fd9150d6b730efc0ff5213329b9f4f
Albat93 <9929@holbertonstudents.com>
2024-10-28 09:37:36 +0000

    multiplie integer

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - function multiplie two integers and returns the result
+ *@a: integer 1
+ *@b: integer 2
+ * Return: 0 success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
