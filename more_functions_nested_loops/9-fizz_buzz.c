c009aa31e0e6847d7cb0aad769208d4586be25f4
Albat93 <9929@holbertonstudents.com>
2024-10-28 20:07:57 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,28 +5,20 @@
  *Return: 0 Success
  */
 
-int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
 		if (i % 3 != 0 && i % 5 != 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
