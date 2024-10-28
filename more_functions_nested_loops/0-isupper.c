9a85baadc4295d3c9a27d79eb52691f013743ccd
Albat93 <9929@holbertonstudents.com>
2024-10-28 09:02:55 +0000

    is upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - check if c is lowercase.
+ * @c: check number
+ * Return: - if upper return 1 - else return 0
+ */
+int _islower(int c)
+
+{
+	if (c >= 'A' && c <= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
