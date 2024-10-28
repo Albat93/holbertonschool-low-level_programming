e4988b4087124bc1c1c45520eca7b08ac1a433a3
Albat93 <9929@holbertonstudents.com>
2024-10-28 09:06:03 +0000

    is upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
+#include <ctype.h>
 #include "main.h"
 /**
  * _isupper - check if c is lowercase.
  * @c: check number
  * Return: - if upper return 1 - else return 0
  */
-int _islower(int c)
+int _isupper(int c)
 
 {
 	if (c >= 'A' && c <= 'A')
