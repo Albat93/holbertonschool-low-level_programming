08f8ba047fbd6202945be83f0ef3569c852403aa
Albat93 <9929@holbertonstudents.com>
2024-10-28 10:43:27 +0000

    print numbers 10x

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers ten times
+ */
+
+void more_numbers(void)
+{
+int c;
+int counter;
+	for (counter = 0; counter <= 14; counter++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
