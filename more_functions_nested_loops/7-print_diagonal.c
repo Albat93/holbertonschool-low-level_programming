d30359158ddc145d245f44a27184359857bd13f0
Albat93 <9929@holbertonstudents.com>
2024-10-28 14:29:16 +0000

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,21 @@
  */
 void print_diagonal(int n)
 {
-        int d, j;
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (d = 0; d < n; d++)
-                {
-                        for (j = 0; j < d; j++)
-                                {
-                                _putchar(' ');
-                                }
-                        _putchar(92);
-                        _putchar('\n');
-                }
-        }
+	int d, j;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (d = 0; d < n; d++)
+		{
+for (j = 0; j < d; j++)
+{
+	_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
 }
