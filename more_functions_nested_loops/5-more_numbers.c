2068d6c0d8096c490d08704c28cd449a23a022ad
Albat93 <9929@holbertonstudents.com>
2024-10-28 12:33:12 +0000

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
  * more_numbers - print numbers ten times
- */
-
+*/
 void more_numbers(void)
 {
-int c;
-int counter;
-	for (counter = 0; counter <= 14; counter++)
-	{
-		for (c = 0; c <= 14; c++)
+	int c = 0;
+	int num;
+
+	while (c <= 9)
 		{
-			_putchar(c + '0');
+		for (num = 0; num <= 9; num++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
+		c++;
 	}
 }
