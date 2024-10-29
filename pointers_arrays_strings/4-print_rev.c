54c1c5a88fc5b0c650b2275214e879d1bd5b829c
Albat93 <9929@holbertonstudents.com>
2024-10-29 17:46:21 +0000

    print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/**
+ *print_rev - prints a string, in reverse, followed by a new line
+ *@s : pointer tested
+ *Return: Always 0.
+*/
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+		{
+		length++;
+		s++;
+		}
+
+	s--;
+
+	while (length > 0)
+	{
+		_putchar(*s);
+		s--;
+		length--;
+	}
+
+	_putchar('\n');
+}
