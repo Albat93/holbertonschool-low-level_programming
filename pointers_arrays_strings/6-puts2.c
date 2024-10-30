bf4aa3cd2281607a84991339c71721c34d8a574f
Albat93 <9929@holbertonstudents.com>
2024-10-30 12:35:18 +0000

    puts 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+#include "3-puts.c"
+/**
+ * puts2 - print every other character of a string
+ * @i: function to start from first character
+ * @str: character tested
+ */
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+
+	for (i = 0; str[i] != '\0' ; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
