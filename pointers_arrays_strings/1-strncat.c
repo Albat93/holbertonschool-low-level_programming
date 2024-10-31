66eca6d128174182d88f7892398a82223600c2a1
Albat93 <9929@holbertonstudents.com>
2024-10-31 13:34:13 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
  * _strncat - cacatenate two strings
  * @dest: destination
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; i < n && dest[i] != '\0';)
+	for (i = 0; dest[i] != '\0';)
 		i++;
 
-	for (j = 0; i < n && src[j] != '\0'; j++, i++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
 
 return (dest);
