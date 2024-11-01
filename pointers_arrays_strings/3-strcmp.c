f74d8bf4dd71110974f74c233d9c50e8820311af
Albat93 <9929@holbertonstudents.com>
2024-11-01 09:09:49 +0000

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ *@s1: string 1
+ *@s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '_0')
+	{
+		if (*s1 - *s2)
+		{
+			return (*s1 -*s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+}
