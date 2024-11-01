25ebb5d82c776d7194300cd716a55060ed1ec2b3
Albat93 <9929@holbertonstudents.com>
2024-11-01 12:00:39 +0000

    change lower to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - function to change lower to upper
+ * @str: string to test
+ * Return: str
+ */
+
+char string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
