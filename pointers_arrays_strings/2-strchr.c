362ea0d58b8fcc3735d56cafdf33e70031ef4f2f
Albat93 <9929@holbertonstudents.com>
2024-11-04 14:44:57 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locates character in a string
  * @s: pointer to the string
  * @c: character to locate
- * Return: s
+ * Return: return to the string in pointer s
  */
 
 char *_strchr(char *s, char c)
