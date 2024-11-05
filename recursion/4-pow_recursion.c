6c78e9a9acb8e9279fcaa6d88caff82b59b7e95e
Albat93 <albat93120@gmail.com>
2024-11-05 15:42:52 +0100

    pow recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,7 +2,9 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: 
+ * @x: number to check
+ * @y: number to check
+ * Return: returns the value of x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
