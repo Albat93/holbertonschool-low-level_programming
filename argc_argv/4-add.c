5c5c034308f919ebd9f9d9e50d03270a7d2765c1
Albat93 <albat93120@gmail.com>
2024-11-06 15:32:46 +0100

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - additionne des nombres positifs passés en argument
+ * @argc: Le nombre d'arguments
+ * @argv: Le tableau des arguments
+ * Return: 0 si succès, 1 si erreur
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
