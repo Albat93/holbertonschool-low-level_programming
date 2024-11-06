60adc03d9034144fa3b402642566c2a6ef96f3a3
Albat93 <albat93120@gmail.com>
2024-11-06 09:33:59 +0100

    puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,7 +3,6 @@
 /**
  * _puts_recursion - print string and trailing new line
  * @s: pointer to the string
- * Return: 0
  */
 
 void _puts_recursion(char *s)
@@ -11,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-	        return;
+		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
