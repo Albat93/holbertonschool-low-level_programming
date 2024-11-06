1b80374876080b86270e0986776d57a826a1bd5c
Albat93 <albat93120@gmail.com>
2024-11-06 09:35:20 +0100

    print rev recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * _print_rev_recursion -
+ * _print_rev_recursion - print string in reverse
  * @s: pointer to string
- * Return: 0
  */
 
 void _print_rev_recursion(char *s)
