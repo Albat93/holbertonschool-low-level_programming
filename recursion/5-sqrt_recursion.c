95d52e1bbb9bf2577f537a7494f371d489f41c04
Albat93 <albat93120@gmail.com>
2024-11-06 10:41:59 +0100

    sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,3 +1,30 @@
 #include "main.h"
 
-int _sqrt_recursion(int n)
+int sqrt_helper(int n, int low, int high)
+{
+	int mid = (low + high) / 2;
+	int mid_squared = mid * mid;
+
+	if (mid_squared == n)
+	{
+		return mid;
+	}
+	else if (mid_squared < n)
+	{
+		return sqrt_helper(n, mid + 1, high);
+	}
+	else
+	{
+		return sqrt_helper(n, low, mid - 1);
+	}
+}
+	int _sqrt_recursion(int n)
+{
+	{
+		if (n < 0)
+		{
+			return -1;
+		}
+		return sqrt_helper(n, 0, n);
+	}
+}
