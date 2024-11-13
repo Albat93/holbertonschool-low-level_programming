f9c853668152698a5990a4697db1ea6290676439
Albat93 <albat93120@gmail.com>
2024-11-13 13:03:43 +0100

    string nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: firts string
+ * @s1: first string
  * @s2: second string
  * @n: limit of s2
  * Return: pointer to new space in memory or null
@@ -12,10 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i;
-	unsigned j;
+	unsigned int j;
 	char *ptr;
 
-	if ((s1 == NULL) && (s2 == NULL))
+	if ((s1 == NULL) || (s2 == NULL))
 		s1 = "", s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
@@ -23,7 +23,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc(sizeof(char) * (i + n + 1));
 
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
