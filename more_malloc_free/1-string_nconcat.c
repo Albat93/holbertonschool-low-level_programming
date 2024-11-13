1e9a53644b661d63847c34eb3d3312813f05fb88
Albat93 <albat93120@gmail.com>
2024-11-13 14:51:43 +0100

    string concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j;
 	char *ptr;
 
-	if ((s1 == NULL) || (s2 == NULL))
-		s1 = "", s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		 s2 = "";
 
 	while (s1[i] != '\0')
 		i++;
