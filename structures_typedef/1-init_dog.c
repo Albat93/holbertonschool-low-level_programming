e3ad09ecda78e85c354a38178ca804aab0dbd444
Albat93 <albat93120@gmail.com>
2024-11-14 15:17:18 +0100

    init dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,6 +7,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -21,5 +22,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 	}
-
 }
