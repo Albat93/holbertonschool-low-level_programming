eaa8f0101ae3ac23e35556fd5a88d23bc41ed5d2
Albat93 <albat93120@gmail.com>
2024-11-15 14:13:00 +0100

    betty is the new owner of poppy

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -4,16 +4,14 @@
 /**
  * free_dog - function to set the dog free.
  * @d: poppy need a new owner because he's mean
- * poor poppy.
  */
 
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-		free (d->name);
-		free (d->owner);
-		free (d);
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
-	return;
 }
