504a982b2b13e6f20b32b4d902982352b52b7736
Albat93 <albat93120@gmail.com>
2024-11-20 13:43:14 +0100

    variadic.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
+#ifndef VAR_h
+#define VAR_h
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
