1ba07221c3c6966f15f53b0cb308158ef76c0aff
Albat93 <albat93120@gmail.com>
2024-11-20 21:07:58 +0100

    add condition for name = null

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 /**
  * print_name - a function prints a name
- * @name: nome of function
+ * @name: name of function
  * @f: pointer of function
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)
+		return;
+	if (name == NULL)
 		return;
 
 	f(name);
