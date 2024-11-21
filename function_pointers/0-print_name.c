5523117685e812dbcabb2c3d6b65fec26ea24768
Albat93 <albat93120@gmail.com>
2024-11-21 13:18:24 +0100

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
+
 /**
  * print_name - print a name
  * @name: name of the person
  * @f: function pointer
- * Return: Nothing
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL || name == NULL)
