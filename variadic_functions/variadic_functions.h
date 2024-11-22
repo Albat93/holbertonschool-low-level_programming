fd86624fc895f4be9511fb469cd08f0fe2e9f437
Albat93 <albat93120@gmail.com>
2024-11-22 12:23:56 +0100

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,23 @@
 #ifndef VAR_h
 #define VAR_h
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct type - type of the structure
+ *
+ * @spec: specifier string
+ * @f: function associated
+ */
+
+typedef struct type
+{
+	char *spec;
+	void (*f)(va_list ap);
+
+} print_type;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
