d6902dcfcd3efa43832f65f6f570c03b457c2c68
Albat93 <albat93120@gmail.com>
2024-12-03 10:15:48 +0100

    sup condition pour head = 0 l16

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,25 +11,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *last_node = *head;
 
-	if (new_node == NULL)
+	if (new_node == NULL) /*if malloc failed*/
 		return (0);
 
-	if (head == NULL)
-		return (0);
-
+/*set value for new node*/
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
-
+/*if list is empty, make new node the head of the list*/
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	while (last_node->next != NULL)
+	while (last_node->next != NULL) /*traverse to the last node*/
 		last_node = last_node->next;
-
+/*append the new node to the end*/
 	last_node->next = new_node;
 
-	return (last_node);
+	return (new_node); /*return new node added at the end*/
 }
