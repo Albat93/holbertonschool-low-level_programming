d4ab12ce97df22a1654a42f89a03d0bcd133e0b8
Albat93 <albat93120@gmail.com>
2024-12-06 17:46:14 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,13 +8,13 @@
  */
 
 int main(int argc, char *argv[])
-{
-	int file_from, file_to, num;
+{	int file_from, file_to, num;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -36,17 +36,17 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 		if (num == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
+		}
 	}
 	if (close(file_from) == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_from);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 	exit(100);
 	}
 	if (close(file_to) == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_to), exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
