ab29fc0d37ea29b7aafca5ba0cbfd4ee6a5424cd
Albat93 <albat93120@gmail.com>
2024-12-06 10:59:54 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,13 +9,13 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t file;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
 
-	file = creat(filename, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
