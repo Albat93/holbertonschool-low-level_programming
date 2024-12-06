f0524899ca866461298d4a96f6c99f47c0725286
Albat93 <albat93120@gmail.com>
2024-12-06 17:38:55 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,8 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -27,8 +26,7 @@ int main(int argc, char *argv[])
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from);
-		exit(99);
+		close(file_from), exit(99);
 	}
 	while ((num = read(file_from, buffer, 1024)) > 0)
 	{
@@ -43,10 +41,14 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 	}
-	if (close((file_from) == -1) || ((file_to) == -1))
+	if (close(file_from) == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_from, file_to);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_from);
 	exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_to), exit(100);
 	}
 	return (0);
 }
