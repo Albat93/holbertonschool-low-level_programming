9b16f7456082be69df21fe3eb7b202a932119ba5
Albat93 <albat93120@gmail.com>
2024-12-06 17:51:52 +0100

    file cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,11 +35,10 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-		if (num == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+	}
+	if (num == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (close(file_from) == -1)
 	{
