26596c5759ea937986bb808f2dd59f8752677747
Albat93 <albat93120@gmail.com>
2024-12-06 17:40:40 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,10 +36,8 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 		if (num == -1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
-		}
 	}
 	if (close(file_from) == -1)
 	{
